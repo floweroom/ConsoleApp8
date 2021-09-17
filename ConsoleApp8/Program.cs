b9e9cfe0a5b20108c545c9e3a3736daab6f07451
floweroom <karpenko.alena493@gmail.com>
2021-09-17 14:20:02 +0300

    test is in developing

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -4,154 +4,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test3
+namespace ConsoleApp8
 {
-
-    struct Complex
-    {
-        public double re;
-        public double im;
-
-
-
-        public Complex(double re, double im)
+   
+        struct Complex
         {
-            this.re = re;
-            this.im = im;
-        }
-
-        public Complex Plus(Complex x)
-        {
-            Complex y;
-            y.re = re + x.re;
-            y.im = im + x.im;
-            return y;
-        }
+            public double re;
+            public double im;
 
 
 
-        public Complex Minus(Complex u)
-        {
-            Complex h;
-            h.re = re - u.re;
-            h.im = im - u.im;
-            return h;
-        }
-
-        public override string ToString()
-        {
-            return $"{re} + {im}i";
-        }
-    }
-    class Lash
-    {
-        public double re;
-        public double im;
-
-
-
-        public Lash Plus(Lash l)
-
-        {
-            Lash s = new Lash();
-
-
-            s.re = re + l.re;
-            s.im = im + l.im;
-            return s;
-        }
-
-
-        public Lash Minus(Lash w)
-
-        {
-
-
-            Lash j = new Lash();
-
-            j.re = re - w.re;
-            j.im = im - w.im;
-            return j;
-        }
-
-        public override string ToString()
-        {
-            return $"{re} + {im}i";
-        }
-    }
-    class Program
-
-    {
-        static void Main(string[] args)
-
-
-
-
-        {
-            Lash lash01 = new Lash();
-            lash01.re = 5;
-            lash01.im = 7;
-            Lash lash02 = new Lash();
-            lash02.re = 4;
-            lash02.im = 2;
-            Console.WriteLine("Введите название операции");
-            string answer = Console.ReadLine();
-
-            Lash lash03 = lash01.Plus(lash02);
-            Lash lash04 = lash01.Minus(lash02);
-
-            switch (answer);
+            public Complex(double re, double im)
             {
-                case "+": 
-                Console.WriteLine(lash03);
-                break;
-                case "-": 
-                Console.WriteLine(lash04);
-                break;
+                this.re = re;
+                this.im = im;
+            }
+
+            public Complex Plus(Complex x)
+            {
+                Complex y;
+                y.re = re + x.re;
+                y.im = im + x.im;
+                return y;
+            }
+
+
+
+            public Complex Minus(Complex u)
+            {
+                Complex h;
+                h.re = re - u.re;
+                h.im = im - u.im;
+                return h;
+            }
+
+            public override string ToString()
+            {
+                return $"{re} + {im}i";
+            }
+        }
+
+        class Program
+        {
+            static void Sum(string[] args)
+            {
+                int i = 12;
+                Complex complex01 = new Complex();
+                complex01.re = 3;
+                complex01.im = 2;
+
+                Complex complex02 = new Complex();
+                complex02.re = -2;
+                complex02.im = 7;
+
+                Complex complex03 = complex01.Plus(complex02);
+
+                Console.WriteLine(complex03);
+                Console.ReadLine();
+
 
 
             }
-        }
-    }
-    class Program1
-    {l
-        static void myMain(string[] args)
-        {
-            int a;
-            int b = 0;
-
-
-            do
+            static void Min()
             {
-                Console.WriteLine("Выведите число на экран");
 
-                int.TryParse(Console.ReadLine(), out a);
+                Complex complex04 = new Complex();
+                complex04.re = 3;
+                complex04.im = 2;
 
-                if (a % 2 != 0)
+                Complex complex05 = new Complex();
+                complex05.re = -2;
+                complex05.im = 7;
+
+                Complex complex06 = complex04.Minus(complex05);
+                Console.WriteLine(complex06);
+                Console.ReadLine();
+
+            }
+
+            class Lash
+            {
+                public double rre;
+                public double iim;
+                public Lash(double rre, double iim);
+
+          {
+                this.rre = rre;
+                this.iim = iim;
+               
+        }
+            public Lash Plus(Lash l)
+
+            {
+                Lash s;
+                s.rre = rre + l.rre;
+                s.iim = iim + l.iim;
+                return s;
+
+            }
+
+            public Lash Minus(Lash w)
+
+            {
+                Lash j;
+                j.rre = rre - w.rre;
+                j.iim = iim - w.iim;
+                return j;
+
+            }
+            public override string ToString()
+            {
+                return $"{rre} + {iim}i";
+            }
+            class Program
+            {
+                static void Main()
                 {
-                    b = b + a;
-                    Console.WriteLine(b);
+
+
+                    Lash lash01 = new Lash(3, 2);
+
+                    int i = 12;
+                    Lash lash01 = new Lash();
+                    lash01.rre = 3;
+                    lash01.iim = 2;
+
+                    Lash lash02 = new Lash(-2, 7);
+                    lash02.rre = -2;
+                    lash02.iim = 7;
+                    lash03 = lash01.Plus(lash02);
+
+                    Console.WriteLine(lash03);
+                    Console.ReadLine();
 
                 }
-                else
+
+
+                static void Minus(string[] args)
                 {
-                    Console.WriteLine("Вы ввели четное число");
+
+                    Lash lash04 = new Lash(3, 2); ;
+                    lash04.rre = 3;
+                    lash04.iim = 2;
+
+                    Lash lash05 = new Lash(-2, 7);
+                    lash05.rre = -2;
+                    lash05.iim = 7;
+
+                    lash06 = lash04.Minus(lash05);
+
+                    Console.WriteLine(lash06);
+                    Console.ReadLine();
+
+
 
                 }
             }
-            while (a != 0);
-        }
-    }
 }
-
-
-                
-
-
-
-            
-
-
-        
-    
-
